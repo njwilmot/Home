d71c440b32625ddc2a867b728fa9575decbb7dbf
njwilmot <79833856+njwilmot@users.noreply.github.com>
2024-02-25 19:57:38 -0700

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -3,64 +3,48 @@ let iterations = 45;
 let cells = 100;
 let cellSize = 5;
 let cellsArray;
-let ruleSlider, ruleDisplay;
+let ruleDisplay;
 
 function setup() {
-  createCanvasEnvironment();
-  initializeCells();
-  setupSlider();
+  let canvas = createCanvas(cells * cellSize, iterations * cellSize);
+  canvas.parent('cellular-automaton');
+  updateCells();
+  ruleSlider = createSlider(0, 255, rule, 1);
+  ruleSlider.parent('cellular-automaton');
+  ruleSlider.input(updateRule);
+  ruleDisplay = createP(`Rule: ${rule}`);
+  ruleDisplay.parent('cellular-automaton');
 }
 
 function draw() {
-  drawCells();
-}
-
-function createCanvasEnvironment() {
-  let canvas = createCanvas(cells * cellSize, iterations * cellSize);
-  canvas.parent('cellular-automaton');
-}
-
-function setupSlider() {
-  ruleSlider = createSlider(0, 255, rule, 1).parent('cellular-automaton').input(updateRule);
-  ruleDisplay = createP(`Rule: ${rule}`).parent('cellular-automaton');
-}
-
-function drawCells() {
   background(255);
-  cellsArray.forEach((row, i) => {
-    row.forEach((cell, j) => {
-      if (cell) fill(0); else fill(255);
-      rect(j * cellSize, i * cellSize, cellSize, cellSize);
-    });
-  });
+  for (let i = 0; i < iterations; i++) {
+    for (let j = 0; j < cells; j++) {
+      if (cellsArray[i][j]) {
+        fill(0);
+        rect(j * cellSize, i * cellSize, cellSize, cellSize);
+      }
+    }
+  }
 }
 
 function updateRule() {
   rule = ruleSlider.value();
   ruleDisplay.html(`Rule: ${rule}`);
-  initializeCells();
+  updateCells();
+  redraw();
 }
 
-function initializeCells() {
-  cellsArray = Array.from({ length: iterations }, () => new Uint8Array(cells));
+function updateCells() {
+  cellsArray = new Array(iterations).fill().map(() => new Array(cells).fill(0));
   cellsArray[0][Math.floor(cells / 2)] = 1;
-  generateCells();
-}
-
-function generateCells() {
   for (let i = 1; i < iterations; i++) {
     for (let j = 0; j < cells; j++) {
-      cellsArray[i][j] = computeCellState(i, j);
+      let left = cellsArray[i - 1][(j - 1 + cells) % cells];
+      let center = cellsArray[i - 1][j];
+      let right = cellsArray[i - 1][(j + 1) % cells];
+      let pattern = (left << 2) | (center << 1) | right;
+      cellsArray[i][j] = (rule >> pattern) & 1;
     }
   }
-  noLoop(); // Stop draw loop if the automaton doesn't need to be dynamically updated.
 }
-
-function computeCellState(i, j) {
-  let left = cellsArray[i - 1][(j - 1 + cells) % cells];
-  let center = cellsArray[i - 1][j];
-  let right = cellsArray[i - 1][(j + 1) % cells];
-  let pattern = (left << 2) | (center << 1) | right;
-  return (rule >> pattern) & 1;
-}
-
